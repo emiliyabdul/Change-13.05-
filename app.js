b169a54e4b45217c18eb7ad2571ceeb596bcdca9
emiliyabdul <emiliyabdul@gmail.com>
2023-05-13 22:21:30 +0400

    dynamic

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let DollarToAzn = 1.7
+let DollarToTl = 19.59
+let DollarToEuro = 0.91
+let DollarToDirhem = 3.67
+
 const currentvalue = document.querySelector("#inpt")
 const ValuetoAZN = document.querySelector("#azn")
 const ValuetoTL = document.querySelector("#tl")
@@ -6,13 +11,13 @@ const ValuetoDIRHEM = document.querySelector("#dirhem")
 const btnCalculate = document.querySelector("#Change")
 
 const Change = () => {
-    ValuetoAZN.innerText = currentvalue.value*1.7
-    ValuetoTL.innerText = currentvalue.value*19.59
-    ValuetoEURO.innerText = currentvalue.value*0.91
-    ValuetoDIRHEM.innerText = currentvalue.value*3.67
+    ValuetoAZN.innerText = currentvalue.value * DollarToAzn
+    ValuetoTL.innerText = currentvalue.value * DollarToTl
+    ValuetoEURO.innerText = currentvalue.value * DollarToEuro
+    ValuetoDIRHEM.innerText = currentvalue.value * DollarToDirhem
 }
 
 
 
 //btnCalculate.addEventListener("click",Change)
-currentvalue.addEventListener("input",Change)
+currentvalue.addEventListener("input", Change)
